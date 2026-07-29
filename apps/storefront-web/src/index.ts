65c4ce4cabcc64cefb71d8998e33dafba1c25d09
rahmatullahboss <rahmatullahzisan01@gmail.com>
2026-07-30 02:12:45 +0600

    feat(storefront): export worker shell boundaries

--- apps/storefront-web/src/index.ts
+++ apps/storefront-web/src/index.ts
@@ -9,9 +9,27 @@ export {
   type StorefrontRuntimeStage,
 } from "./environment.js";
 export {
+  createStorefrontHostResolver,
+  createStorefrontTransportResolver,
+  type StorefrontHostResolver,
+  type StorefrontResolveOptions,
+} from "./host-resolver.js";
+export {
+  storefrontShellResponse,
+  type StorefrontShellRenderOptions,
+} from "./render.js";
+export {
   resolveStorefrontRequest,
   storefrontHealthResponse,
   storefrontRequestHostname,
+  storefrontServiceUnavailableResponse,
   storefrontUnavailableResponse,
   type StorefrontResolvedRequest,
 } from "./runtime.js";
+export {
+  createStorefrontWorker,
+  type StorefrontResolverFactory,
+  type StorefrontWorker,
+  type StorefrontWorkerBindings,
+  type StorefrontWorkerOptions,
+} from "./worker.js";
