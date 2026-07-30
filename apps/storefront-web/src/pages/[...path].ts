721af760c43023b845b19b00bd6a968ffe0bbff6
rahmatullahboss <rahmatullahzisan01@gmail.com>
2026-07-30 17:03:45 +0600

    feat(storefront): intercept robots and sitemap requests

--- apps/storefront-web/src/pages/[...path].ts
+++ apps/storefront-web/src/pages/[...path].ts
@@ -1,10 +1,14 @@
 import type { APIRoute } from "astro";
 import { env } from "cloudflare:workers";
+import { handleStorefrontSeoRoute } from "../seo-route.js";
 import storefrontWorker, {
   type StorefrontWorkerBindings,
 } from "../worker.js";
 
 export const prerender = false;
 
-export const ALL: APIRoute = async ({ request }) =>
-  storefrontWorker.fetch(request, env as StorefrontWorkerBindings);
+export const ALL: APIRoute = async ({ request }) => {
+  const bindings = env as StorefrontWorkerBindings;
+  const seoResponse = await handleStorefrontSeoRoute(request, bindings);
+  return seoResponse ?? storefrontWorker.fetch(request, bindings);
+};
