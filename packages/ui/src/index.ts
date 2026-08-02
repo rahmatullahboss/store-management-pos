955e9788749cc8d4b0f94719b41f1b73cca48df9
rahmatullahboss <rahmatullahzisan01@gmail.com>
2026-08-03 05:33:01 +0600

    feat(ui): export Stitch screen authority registry

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,5 +2,6 @@ export * from "./app-shell.js";
 export * from "./direction-support.js";
 export * from "./foundation-reference.js";
 export * from "./operations-ledger.js";
+export * from "./operations-ledger-screens.js";
 export * from "./rendering.js";
 export * from "./api-client.js";
